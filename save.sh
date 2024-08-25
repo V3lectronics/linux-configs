851f1902242759dc1b2d158deb159279a522f7b7
barbarjan <niszczyciel.agawa@gmail.com>
2024-08-25 19:17:20 +0200

    automatically udpate config files

--- save.sh
+++ save.sh
@@ -2,7 +2,7 @@
 
 USER_HOME='/home/jan'
 
-echo 'Updating config files... '
+echo '========== Updating config files... =========='
 cp $USER_HOME/.zshrc .
 cp $USER_HOME/.config/bspwm/bspwmrc .
 cp $USER_HOME/.config/sxhkd/sxhkdrc .
@@ -12,10 +12,10 @@ cp $USER_HOME/.config/flameshot/flameshot.ini .
 cp $USER_HOME/.config/ranger/rc.conf .
 cp -r $USER_HOME/.config/nvim .
 
-echo 'Updating installed packages list... '
+echo '========== Updating installed packages list... =========='
 apt list --installed > installed_apt_packages.txt
 
-echo 'Commiting updates to git... '
+echo '========== Commiting updates to git... =========='
 git config --global user.email "niszczyciel.agawa@gmail.com"
 git config --global user.name "barbarjan"
 git add .
@@ -23,5 +23,5 @@ git restore --staged .git
 git commit -m 'automatically udpate config files'
 # git push
 
-echo 'Finished... '
+echo '========== Finished... =========='
 echo "You can 'git push' now"
