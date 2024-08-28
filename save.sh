fe69874c85bbf9de8a99de908ec4a8c8cb67755a
barbarjan <niszczyciel.agawa@gmail.com>
2024-08-28 21:09:55 +0200

    automatically udpate config files

--- save.sh
+++ save.sh
@@ -11,9 +11,11 @@ cp $USER_HOME/.config/polybar/config.ini .
 cp $USER_HOME/.config/flameshot/flameshot.ini .
 cp $USER_HOME/.config/ranger/rc.conf .
 cp -r $USER_HOME/.config/nvim .
+echo "[SUCCESS]Copied config files to local repository"
 
 echo '========== Updating installed packages list... =========='
 apt list --installed > installed_apt_packages.txt
+echo "[SUCCESS]Updated list installed_apt_packages.txt"
 
 echo '========== Commiting updates to git... =========='
 git config --global user.email "niszczyciel.agawa@gmail.com"
@@ -22,6 +24,7 @@ git add .
 # git restore --staged .git
 git commit -m 'automatically udpate config files'
 # git push
+echo "[SUCCESS]Commit created"
 
 echo '========== Finished =========='
-echo "You can 'git push' now"
+echo "You can do 'git push' now"
