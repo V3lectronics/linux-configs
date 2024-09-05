fc9ee34944f6d8409af685b146266eea525e3cdd
barbarjan <niszczyciel.agawa@gmail.com>
2024-09-05 16:12:06 +0200

    automatically udpate config files

--- nvim/lua/jan/remap.lua
+++ nvim/lua/jan/remap.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "<C-l>", "<Right>")
--- vim.keymap.set("i", "<C-h>", "<Left>")
-vim.keymap.set("i", "<C-h>", "<Esc>i")
+vim.keymap.set("i", "<C-h>", "<Left>")
 vim.keymap.set("i", "<C-j>", "<Down>")
 vim.keymap.set("i", "<C-k>", "<Up>")
